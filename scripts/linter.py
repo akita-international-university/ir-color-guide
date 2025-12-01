422bc8e73bf01a395f6e5177f46598456f981cd2
ttsukagoshi <taro.tsukagoshi@gmail.com>
2025-12-01 18:33:59 +0900

    Add formatter and linter scripts for code formatting and linting

---
+++ scripts/linter.py
@@ -0,0 +1,26 @@
+"""
+Script to run linters after running formatters defined in scripts.formatter.py.
+"""
+
+import subprocess
+
+# Lint対象のディレクトリ
+LINT_TARGET = ["./scripts"]  # , "./tests"]
+
+
+def main():
+    """
+    Run linters after running the set of formatters defined in scripts.formatter.py.
+    """
+    # First, run the formatter
+    subprocess.run(["poetry", "run", "formatter"], check=True)
+    for target in LINT_TARGET:
+        # Run mypy
+        print(f"Linting {target} with mypy...")
+        subprocess.run(["poetry", "run", "mypy", target], check=True)
+        print("Linting done.")
+        # Run pylint
+        print(f"Linting {target} with pylint...")
+        subprocess.run(["poetry", "run", "pylint", target], check=True)
+        print("Linting done.")
+    print("All linting complete.")
