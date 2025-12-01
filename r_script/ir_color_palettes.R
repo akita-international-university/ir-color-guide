98c2a4c43b68321afdb23d4a92ac5a266b78ef82
ttsukagoshi <taro.tsukagoshi@gmail.com>
2025-12-01 16:49:02 +0900

    Add ./r_script/ir_color_palettes.R based on current palettes.yml to use as a sample for defining the conversion script

---
+++ r_script/ir_color_palettes.R
@@ -0,0 +1,33 @@
+# Color palettes based on the IR Data Visualization Color Guidelines
+# This file is created automatically. Do NOT edit manually.
+# See: https://github.com/akita-international-university/ir-color-guide
+
+color_values_aiu_grades <- c(
+    # Type: Categorical
+    # Description: Colors for AIU letter grades
+    "A+" = "#24693d",
+    "A" = "#519c51",
+    "A-" = "#b3e0a6",
+    "B+" = "#2c5985",
+    "B" = "#3b96b2",
+    "B-" = "#bce4d8",
+    "C+" = "#7c4d79",
+    "C" = "#b27ba1",
+    "C-" = "#eec9e5",
+    "D+" = "#ca5422",
+    "D" = "#ffc685",
+    "F" = "#d3293d",
+    "P" = "#5b8cb8",
+    "F*" = "#ffbeb2",
+    "AP" = "#4e79a7"
+)
+
+color_values_aiu_admission_types <- c(
+    # Type: Categorical
+    # Description: Colors for the exam types at AIU
+    "A" = "#2a5783",
+    "B" = "#5b8cb8",
+    "C" = "#b9ddf1",
+    "GS" = "#24693d",
+    "GW" = "#b3e0a6"
+)
