799fe59f2e8b09ba69180d16fedbdc722a980316
ttsukagoshi <taro.tsukagoshi@gmail.com>
2025-12-02 16:32:28 +0900

    Add palette: In-School Sems & Course Levels

--- r_script/ir_color_palettes.R
+++ r_script/ir_color_palettes.R
@@ -31,3 +31,40 @@ color_values_aiu_exam_types <- c(
     "GS" = "#24693d",
     "GW" = "#b3e0a6"
 )
+
+color_values_aiu_inschool_semesters <- c(
+    # Type: Categorical
+    # Description: Colors for the in-school semesters at AIU
+    "0" = "#1ba3c6",
+    "1" = "#2db7c0",
+    "2" = "#2fbaa9",
+    "3" = "#1fae81",
+    "4" = "#38a452",
+    "5" = "#6ca932",
+    "6" = "#bcbd22",
+    "7" = "#e4ba21",
+    "8" = "#f8a61c",
+    "9" = "#f88113",
+    "10" = "#e74b21",
+    "11" = "#e6343b",
+    "12" = "#f43c63",
+    "13" = "#fa6692",
+    "14" = "#f075b0",
+    "15" = "#d669be",
+    "16" = "#a76dc2",
+    "Exchange" = "#9f3632",
+    "Other" = "#59504e"
+)
+
+color_values_aiu_course_levels <- c(
+    # Type: Categorical
+    # Description: Colors for the course levels at AIU
+    "700" = "#b9ca5d",
+    "600" = "#f1788d",
+    "500" = "#8fb202",
+    "400" = "#cf3e53",
+    "300" = "#f3a546",
+    "200" = "#b9ddf1",
+    "100" = "#5b8cb8",
+    "0" = "#2a5783"
+)
